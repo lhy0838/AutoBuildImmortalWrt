e654b98611e737fb1218d797d40e14161eea6758
lhy0838 <58759648+lhy0838@users.noreply.github.com>
2025-04-06 20:33:58 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 # 99-custom.sh 就是immortalwrt固件首次启动时运行的脚本 位于固件内的/etc/uci-defaults/99-custom.sh
 # Log file for debugging
@@ -70,9 +71,9 @@ elif [ "$count" -gt 1 ]; then
    # LAN口设置静态IP
    uci set network.lan.proto='static'
    # 多网口设备 支持修改为别的ip地址
-   uci set network.lan.ipaddr='192.168.100.1'
+   uci set network.lan.ipaddr='192.168.199.1'
    uci set network.lan.netmask='255.255.255.0'
-   echo "set 192.168.100.1 at $(date)" >> $LOGFILE
+   echo "set 192.168.199.1 at $(date)" >> $LOGFILE
    # 判断是否启用 PPPoE
    echo "print enable_pppoe value=== $enable_pppoe" >> $LOGFILE
    if [ "$enable_pppoe" = "yes" ]; then
