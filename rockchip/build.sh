2559d0da68e87e10d913482cf442e7b352cd75d6
lhy0838 <58759648+lhy0838@users.noreply.github.com>
2025-04-09 15:31:40 +0800

    Update build.sh

--- rockchip/build.sh
+++ rockchip/build.sh
@@ -47,6 +47,7 @@ PACKAGES="$PACKAGES luci-i18n-wol-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-alist-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 PACKAGES="$PACKAGES htop"
+PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
