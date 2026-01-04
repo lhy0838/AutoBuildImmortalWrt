94bcb95096e6fcaedd676458f5d418330056ff6a
lhy0838 <58759648+lhy0838@users.noreply.github.com>
2026-01-04 16:08:44 +0800

    Update build24.sh

--- rockchip/build24.sh
+++ rockchip/build24.sh
@@ -73,7 +73,7 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 
 #备选部分
 PACKAGES="$PACKAGES luci-i18n-netdata-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
+#PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
